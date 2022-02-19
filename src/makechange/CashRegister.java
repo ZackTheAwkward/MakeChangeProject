47cf0f2d177020916e856263760eaedc9b7725c5
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-02-18 17:53:45 -0700

    declared scanner in cashregister

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -1,10 +1,11 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class CashRegister {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
 	}
 
 }
