ee320814a416ffeff52897182767ef853c965876
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-02-19 17:22:58 -0700

    project/README complete

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### Make Change Project
 
 #### Description
-This program is designed to issue change back to the user after making a purchase.
+This program is designed to issue change back to the user after making a purchase. Listing out  
 #### Lessons Learned
+The modulus command cannot compile decimals, this was proven difficult for returning change, but after a simple conversion, the code was able to compile with no system errors.
 
+Also when trying to get change in pennies, the modulus would return a decimal like 1.9, so switching the modulus from a double to an int in the final steps was not a component I though about until I realized my program was trying to short change my customers. 
 #### Technologies Used
+Operations, Scanners, System outputs, if statements, and conversions from double to int.
--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -6,6 +6,67 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-	}
 
+		double itemPrice, payTend;
+		System.out.println("What is the price of them item? ");
+		itemPrice = scanner.nextDouble();
+
+		System.out.println("What was the amount tendered? ");
+		payTend = scanner.nextDouble();
+
+		if (itemPrice > payTend) {
+			System.out.println("Incorrect amount entered, please try again");
+
+		} else if (itemPrice == payTend) {
+			System.out.println("Exact change");
+
+		} else if (payTend > itemPrice) {
+
+			double changeDue;
+			changeDue = payTend - itemPrice;
+			int change100 = (int) changeDue / 100;
+			System.out.println("The change in $100 is " + change100);
+			changeDue = changeDue % 100;
+
+			int change50 = (int) changeDue / 50;
+			System.out.println("The change in $50 is " + change50);
+			changeDue = changeDue % 50;
+
+			int change20 = (int) changeDue / 20;
+			System.out.println("The change in $20 is " + change20);
+			changeDue = changeDue % 20;
+
+			int change10 = (int) changeDue / 10;
+			System.out.println("The change in $10 is " + change10);
+			changeDue = changeDue % 10;
+
+			int change5 = (int) changeDue / 5;
+			System.out.println("The change in $5 is " + change5);
+			changeDue = changeDue % 5;
+
+			int change1 = (int) changeDue / 1;
+			System.out.println("The change in $1 is " + change1);
+			changeDue = changeDue % 1;
+
+			changeDue = changeDue * 100;
+			int changeQuart = (int) changeDue / 25;
+			System.out.println("The change in quarters is " + (int) changeQuart);
+			changeDue = changeDue % 25;
+
+			int changeDimes = (int) changeDue / 10;
+			System.out.println("The change in dimes is " + (int) changeDimes);
+			changeDue = changeDue % 10;
+
+			int changeNickels = (int) changeDue / 5;
+			System.out.println("The change in nickels is " + (int) changeNickels);
+			changeDue = (int) changeDue % 5;
+
+			int changePennies = (int) changeDue / 1;
+			System.out.println("The change in pennies is " + (int) changePennies);
+			changeDue = changeDue % 1;
+
+		}
+		scanner.close();
+
+	}
 }
