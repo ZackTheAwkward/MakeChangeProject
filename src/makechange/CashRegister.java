313855b6581d00d6d462cd31374ab5a75e225b04
Zack Gaddy <zack.e.gaddy@gmail.com>
2022-02-23 12:06:38 -0700

    Corrected Code

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -25,49 +25,65 @@ public class CashRegister {
 			double changeDue;
 			changeDue = payTend - itemPrice;
 			int change100 = (int) changeDue / 100;
-			System.out.println("The change in $100 is " + change100);
+			if(change100 != 0) {
+				System.out.println("The change in $100 is " + change100);
+			}
 			changeDue = changeDue % 100;
 
 			int change50 = (int) changeDue / 50;
-			System.out.println("The change in $50 is " + change50);
+			if(change50 != 0) {
+				System.out.println("The change in $50 is " + change50);				
+			}
 			changeDue = changeDue % 50;
 
 			int change20 = (int) changeDue / 20;
-			System.out.println("The change in $20 is " + change20);
+			if(change20 != 0) {
+				System.out.println("The change in $20 is " + change20);	
+			}
 			changeDue = changeDue % 20;
 
 			int change10 = (int) changeDue / 10;
-			System.out.println("The change in $10 is " + change10);
+			if(change10 !=0) {
+				System.out.println("The change in $10 is " + change10);
+			}
 			changeDue = changeDue % 10;
 
 			int change5 = (int) changeDue / 5;
-			System.out.println("The change in $5 is " + change5);
+			if(change5 !=0) {
+				System.out.println("The change in $5 is " + change5);
+			}
 			changeDue = changeDue % 5;
 
 			int change1 = (int) changeDue / 1;
-			System.out.println("The change in $1 is " + change1);
+			if(change1 != 0) {
+				System.out.println("The change in $1 is " + change1);
+			}
 			changeDue = changeDue % 1;
 
-			
-			
 			changeDue = changeDue * 100;
 			changeDue = Math.round(changeDue); 
 			int changeQuart = (int)changeDue / 25;
-			System.out.println("The change in quarters is " + (int) changeQuart);
+			if(changeQuart != 0) {
+				System.out.println("The change in quarters is " + (int) changeQuart);
+			}
 			changeDue = changeDue % 25;
 
 			int changeDimes = (int)changeDue / 10;
-			System.out.println("The change in dimes is " + (int) changeDimes);
+			if(changeDimes != 0) {
+				System.out.println("The change in dimes is " + (int) changeDimes);
+			}
 			changeDue = changeDue % 10;
 
 			int changeNickels = (int)changeDue / 5;
-			System.out.println("The change in nickels is " + (int) changeNickels);
+			if(changeNickels != 0) {
+				System.out.println("The change in nickels is " + (int) changeNickels);
+			}
 			changeDue = (int) changeDue % 5;
 
 			int changePennies = (int)changeDue / 1;
-			System.out.println("The change in pennies is " + (int) changePennies);
-			changeDue = changeDue % 1.0;
-
+			if(changePennies != 0) {
+				System.out.println("The change in pennies is " + (int) changePennies);
+			}
 		}
 		scanner.close();
 
